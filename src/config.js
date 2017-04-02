17554315878220daf9d1fdd7c17a4d716595086e
Traxyco <moojan.asghari@gmail.com>
2017-04-03 01:00:36 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+let config = {}
+config.recast = {}
+config.recast.request_token = '860b8fd068c84cc3218e735ce21efa5e'
+config.recast.language = 'en' // to chose your language 'fr' or 'en'
+config.telegram = {}
+config.telegram.token = '314954027:AAEROrviUg6mC7pBObE8ZYoFlMqGZxnZ0wE'
+module.exports = config
